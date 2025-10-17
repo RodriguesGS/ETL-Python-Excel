e9620b0eda2952f40ddacc02fff993da7789eff9
Gabriel <rodriguesgasoares@gmail.com>
2025-10-17 08:27:27 -0300

    feat: add validações no contrato de dados

---
+++ src/read_csv.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import streamlit as st
+from validator import PlanilhaVendas
+from pydantic import ValidationError
+
+def read_data(df):
+    error = []
+    valid_rows = []
+
+    for index, row in df.iterrows():
+        try:
+            data = row.to_dict()
+            
+            valid_schema = PlanilhaVendas(**data)
+            valid_rows.append(valid_schema)
+            
+        except ValidationError as e:
+            error.append(f"Erro na linha {index + 2}: {str(e)}")
+            
+    return valid_rows, error
+
+def main():
+    st.title("Validação de dados da Campanha")
+    st.write("Faça upload do arquivo CSV para validação dos dados.")
+    
+    uploaded_file = st.file_uploader("Escolha um arquivo CSV", type="csv")
+    
+    if uploaded_file is not None:
+        try:
+            df = pd.read_csv(uploaded_file)
+            
+            st.write("Preview dos dados:")
+            st.dataframe(df.head())
+            
+            if st.button("Validar Dados:"):
+                with st.spinner("Validando..."):
+                    valid_rows, errors = read_data(df)
+                    
+                    if errors:
+                        st.error("Foram encontrados erros na validação:")
+                        for err in errors:
+                            st.write(err)
+                    else:
+                        st.success("Todos os dados são válidos!")
+                    
+                    st.write(f"Número de registros válidos: {len(valid_rows)}")
+                    
+                    valid_df = pd.DataFrame([data.model_dump() for data in valid_rows])
+                    st.download_button(
+                        label="Baixar dados válidos como CSV",
+                        data=valid_df.to_csv(index=False),
+                        file_name='dados_validos.csv',
+                        mime='text/csv'
+                    )
+                    
+        except Exception as e:
+            st.error(f"Erro ao processar o arquivo: {str(e)}")
+            
+if __name__ == "__main__":
+    main()
--- src/validator.py
+++ src/validator.py
@@ -1,20 +1,18 @@
 from pydantic import BaseModel, Field
-from Typing import Optional
+from typing import Optional
 
-
-
-class User(BaseModel):
+class PlanilhaVendas(BaseModel):
     Organizador: int = Field(..., description="ID do Organizador")
     Ano_Mes: str = Field(..., description="Ano e mes do registro")
     Dia_da_Semana: str = Field(..., description="Dia da semana correspondente a data")
-    Tipo_dia: str = Field(..., description="Tipo do dia (util, fim de semana, feriado)")
+    Tipo_Dia: str = Field(..., description="Tipo do dia (util, fim de semana, feriado)")
     Objetivo: str = Field(..., description="Objetivo da campanha ou ação")
     Date: str = Field(..., description="Data do registro no formato YYYY-MM-DD")
     AdSet_name: Optional[str] = Field(None, description="Nome do conjunto de anúncios")
-    Amount_spent: float = Field(0.0, description="Valor gasto na campanha")
-    Link_clicks: Optional[int] = Field(None, description="Número de cliques no link")
-    Impressions: int = Field(0, description="Número de impressões")
-    Conversions: Optional[int] = Field(None, description="Número de conversões")
-    Segmentacao: Optional[str] = Field(None, description="Segmentação utilizada na campanha")
-    Tipo_de_Anuncio: str = Field(..., description="Tipo de anúncio utilizado")
+    Amount_spent: float = Field(0.0, ge=0, le=1200.00, description="Valor gasto na campanha")
+    Link_clicks: Optional[float] = Field(None, description="Número de cliques no link", nullable=True)
+    Impressions: int = Field(0, description="Número de impressões", nullable=True)
+    Conversions: Optional[float] = Field(None, description="Número de conversões", nullable=True)
+    Segmentacao: Optional[float] = Field(None, description="Segmentação utilizada na campanha")
+    Tipo_de_Anúncio: str = Field(..., description="Tipo de anúncio utilizado")
     Fase: str = Field(..., description="Fase da campanha")
